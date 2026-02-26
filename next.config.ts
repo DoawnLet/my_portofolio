99f10d03f6ab0e699ba5208d04795f46525495ae
Vũ Trần Bình Minh <binhminhvutran@gmail.com>
2026-02-26 16:09:33 +0700

    Fix error deploy

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/my_portofolio",
-  assetPrefix: "/my_portofolio/",
-  images: {
-    unoptimized: true, // Next.js image optimization API doesn't work in static export
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
