10120ad9c08027136c07992cfe761da319363d8e
Vũ Trần Bình Minh <binhminhvutran@gmail.com>
2026-02-26 15:39:14 +0700

    Prepare for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/my_portofolio",
+  assetPrefix: "/my_portofolio/",
+  images: {
+    unoptimized: true, // Next.js image optimization API doesn't work in static export
+  },
 };
 
 export default nextConfig;
